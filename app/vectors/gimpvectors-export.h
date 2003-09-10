48b12dfc105e284c131a65a14da70afcf67d28a3
neo <neo@f57ce169-c725-0410-9505-c4955b3f21d2>
2003-09-10 13:05:00 +0000

    2003-09-10  Sven Neumann  <sven@gimp.org>
    	* app/vectors/Makefile.am
    	* app/vectors/gimpvectors-export.[ch]
    	* app/vectors/gimpvectors-import.[ch]: added stubs for
    	import/export of paths.
    
    	* app/vectors/gimpvectors-preview.h: cosmetics.
    
    
    git-svn-id: http://svn.gnome.org/svn/gimp/trunk@10629 f57ce169-c725-0410-9505-c4955b3f21d2

--- app/vectors/gimpvectors-export.h
+++ app/vectors/gimpvectors-export.h
@@ -16,17 +16,13 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#ifndef __GIMP_VECTORS_PREVIEW_H__
-#define __GIMP_VECTORS_PREVIEW_H__
+#ifndef __GIMP_VECTORS_EXPORT_H__
+#define __GIMP_VECTORS_EXPORT_H__
 
 
-/*
- *  virtual function of GimpVectors -- dont't call directly
- */
-
-TempBuf * gimp_vectors_get_new_preview (GimpViewable *viewable,
-                                        gint          width,
-                                        gint          height);
+gboolean  gimp_vectors_export (const GimpVectors  *vectors,
+                               const gchar        *filename,
+                               GError            **error);
 
 
-#endif /* __GIMP_VECTORS_PREVIEW_H__ */
+#endif /* __GIMP_VECTORS_IMPORT_H__ */
